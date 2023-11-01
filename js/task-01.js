b3718a470ad7142acd8cfaca550f8d1f3dae8bb6
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-01 18:51:45 +0100

    add task-01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+console.log('task-01');
+
+const logItems = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+};
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+console.log('');
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
