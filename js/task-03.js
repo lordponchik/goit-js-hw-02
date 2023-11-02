a90b978015866da8d69fecb4e859ed7bd0fe10c5
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-02 12:54:51 +0100

    add task-03

---
+++ js/task-03.js
@@ -0,0 +1,9 @@
+console.log('task-03');
+
+const findLongestWord = function (string) {
+  return string.split(' ').sort((a, b) => b.length - a.length)[0];
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('May the force be with you')); // 'force'
